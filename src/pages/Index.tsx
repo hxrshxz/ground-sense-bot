e612205154831ae8cfec4eb32d775b91dd83ef2b
Anushka Sharan <anushkasharan05@gmail.com>
2026-01-09 22:45:52 +0530

    style: modernize landing page with mesh gradients and spotlight effects

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,7 @@
-import { BusinessTools } from "@/components/BusinessTools";
 import { INGRESAssistant } from "@/components/INGRESAssistant";
 
 const Index = () => {
-  return <BusinessTools />;
+  return <INGRESAssistant />;
 };
 
 export default Index;
